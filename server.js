96ef1991ba3b3f6e58fb55678d84d4d4fbfdedac
bparrish206 <brentparrish@www.airport.us>
2015-06-30 22:10:15 -0700

    tweak

--- server.js
+++ server.js
@@ -11,9 +11,8 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static(__dirname + '/build'));
 
-app.set('port', process.env.PORT || 8000);
-app.listen(app.get('port'), function() {
-  console.log('server running on port: %d', app.get('port'));
-});
+
+app.listen(process.env.PORT || 8000);
+
 
 module.exports = app;
